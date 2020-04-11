ffed20b99dcbd3e82e831473fa5c68b29aed6884
MY <715711877@qq.com>
2020-04-11 15:21:56 +0800

    完成上传服务接口设计

---
+++ blb-backend/basic-service-api/upload-service-api/src/main/java/wang/ismy/blb/api/upload/UploadApi.java
@@ -0,0 +1,24 @@
+package wang.ismy.blb.api.upload;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+import wang.ismy.blb.common.Result;
+
+/**
+ * @author MY
+ * @date 2020/4/11 15:20
+ */
+@Api(tags = "上传服务")
+public interface UploadApi {
+
+    /**
+     * 文件上传
+     * @param file
+     * @return
+     */
+    @ApiOperation("文件上传")
+    @PostMapping("")
+    Result<String> upload(MultipartFile file);
+}
