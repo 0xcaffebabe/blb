2e1cc41b15b080e34584555ac331b8199d3f401d
MY <715711877@qq.com>
2020-04-11 15:08:51 +0800

    完成消息服务接口设计

---
+++ blb-backend/basic-service-api/message-service-api/src/main/java/wang/ismy/blb/api/message/Message.java
@@ -0,0 +1,21 @@
+package wang.ismy.blb.api.message;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @author MY
+ * @date 2020/4/11 15:03
+ */
+@Data
+@ApiModel("消息参数")
+public class Message {
+    /** 可选值:email weixin*/
+    @ApiModelProperty(value = "消息类型")
+    private String type;
+    @ApiModelProperty("消息内容")
+    private String content;
+    @ApiModelProperty("邮箱或者openid")
+    private String receiverId;
+}
---
+++ blb-backend/basic-service-api/message-service-api/src/main/java/wang/ismy/blb/api/message/MessageApi.java
@@ -0,0 +1,25 @@
+package wang.ismy.blb.api.message;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import wang.ismy.blb.common.Result;
+
+
+/**
+ * @author MY
+ * @date 2020/4/11 15:03
+ */
+@Api(tags = "消息服务接口")
+public interface MessageApi {
+
+    /**
+     * 发送一条消息
+     * @param message
+     * @return
+     */
+    @ApiOperation("发送一条消息")
+    @PostMapping("")
+    Result<Void> sendMessage(@RequestBody Message message);
+}
