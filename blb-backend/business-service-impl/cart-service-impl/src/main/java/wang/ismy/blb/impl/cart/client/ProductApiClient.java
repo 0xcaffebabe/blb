0739db8c67e6acd0c1aee054edcc32a8c4f25f18
MY <715711877@qq.com>
2020-04-25 12:02:21 +0800

    去除购物车 商品服务mock

--- blb-backend/business-service-impl/cart-service-impl/src/main/java/wang/ismy/blb/impl/cart/client/ProductApiClient.java
+++ blb-backend/business-service-impl/cart-service-impl/src/main/java/wang/ismy/blb/impl/cart/client/ProductApiClient.java
@@ -19,36 +19,6 @@ import java.util.List;
  */
 @FeignClient(value = ServiceName.PRODUCT_SERVICE,fallback = ProductApiClient.Fallback.class)
 public interface ProductApiClient extends ProductApi {
-
-    // TODO 等待商品服务开发完成
-    /** mock 实现*/
     @Component
-    class Fallback implements ProductApiClient{
-
-        @Override
-        public Result<ProductDTO> getProduct(Long productId) {
-            ProductDTO productDTO = MockUtils.create(ProductDTO.class);
-            productDTO.setProductId(productId);
-            ProductCategoryDTO category = MockUtils.create(ProductCategoryDTO.class);
-            category.setShopId(1L);
-            productDTO.setProductCategory(category);
-            return Result.success(productDTO);
-        }
-
-        @Override
-        public Result<List<ProductDTO>> getProductList(List<Long> productIdList) {
-            return null;
-        }
-
-        @Override
-        public Result<List<ProductDTO>> getListByProductAndSpecList(List<CartProductGetDTO> list) {
-            return null;
-        }
-
-        @Override
-        public Result<ProductSpecDTO> getProductSpec(Long specId) {
-            ProductSpecDTO productSpecDTO = MockUtils.create(ProductSpecDTO.class);
-            return Result.success(productSpecDTO);
-        }
-    }
+    class Fallback extends ProductApi.Fallback implements ProductApiClient{ }
 }
