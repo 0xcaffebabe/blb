8fc48557493a4989107b6ebd02976732408c35d1
MY <715711877@qq.com>
2020-05-04 09:36:18 +0800

    完成聚合服务结构设计

---
+++ blb-backend/aggregation-service/consumer-aggregation-service/src/main/java/wang/ismy/blb/aggregation/client/ConsumerApiClient.java
@@ -0,0 +1,16 @@
+package wang.ismy.blb.aggregation.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import wang.ismy.blb.api.consumer.ConsumerApi;
+import wang.ismy.blb.common.enums.ServiceName;
+
+/**
+ * @author MY
+ * @date 2020/5/4 9:21
+ */
+@FeignClient(value = ServiceName.CONSUMER_SERVICE,fallback = ConsumerApiClient.Fallback.class)
+public interface ConsumerApiClient extends ConsumerApi {
+    @Component
+    class Fallback extends ConsumerApi.Fallback implements ConsumerApiClient{}
+}
