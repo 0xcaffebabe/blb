3f42f169744e63981a564dd2eee658a0644be7e3
MY <715711877@qq.com>
2020-10-25 21:19:30 +0800

    更新shop header ui展示

--- blb-consumer-frontend/src/components/shop/ShopHeader.vue
+++ blb-consumer-frontend/src/components/shop/ShopHeader.vue
@@ -1,26 +1,46 @@
 <template>
   <div>
     <el-card class="shop-header" :body-style="{padding:0}">
-      <div class="shop-header-img">
-        <el-image :src="info.shopLogo" fit="cover"/>
-      </div>
       <el-row class="shop-detail" :gutter="20">
-        <el-col :span="4">
+        <el-col :span="8">
           <el-image
             class="shop-logo"
-            style="width: 150px; height: 150px"
+            style="width: 400px; height: 200px"
             :src="info.shopLogo"
             :preview-src-list="[info.shopLogo]"
-            fit="cover"></el-image>
+            fit="cover"/>
+            <ul class="img-list">
+              <li v-for="item in 4" :key="item + '1'">
+                <el-image
+                class="shop-logo"
+                style="width: 92px; height: 50px"
+                :src="info.shopLogo"
+                :preview-src-list="[info.shopLogo]"
+                fit="cover"/>
+              </li>
+            </ul>
         </el-col>
         <el-col :span="16">
-          <h1 class="shop-name">{{info.shopName}}</h1>
+          <h2 class="shop-name">{{info.shopName}}</h2>
+          <el-rate
+            v-model="rate"
+            disabled
+            show-score
+            text-color="#ff9900"
+            score-template="{value}分"
+            class="shop-rate"
+            >
+          </el-rate>
+          <el-divider/>
           <p class="shop-delivery">
             <el-tag effect="dark" type="primary" size="mini" style="vertical-align:top">蜂鸟专送</el-tag>
             / 35分钟送达
              / 配送费￥{{info.deliveryFee}} 起送价 ￥{{info.startingPrice}}
             </p>
+          <p>地址：鲤城区媒人桥路2号 <span class="el-icon-location" style="color:#409EFF"></span> </p>
+          <p>电话：17359801234</p>
           <p class="shop-slogan">{{info.shopSlogan}}</p>
+          <el-divider/>
         </el-col>
       </el-row>
     </el-card>
@@ -31,41 +51,49 @@
 export default {
   props: ['info'],
   data () {
-    return {}
+    return {
+      rate: 3.7
+    }
   }
 }
 </script>
 
 <style lang="less" scoped>
-  .shop-header-img {
-    padding: 0;
-    -webkit-tap-highlight-color: transparent;
-    -webkit-font-smoothing: antialiased;
-    .el-image {
-      width: 100%;
-      height: 200px;
-      filter: blur(10px);
-    }
+  .shop-header {
+    padding: 20px;
+  }
+  .el-image {
+    border-radius: 5px;
   }
   .shop-detail {
-    margin-top: -200px;
-    top: 0;
     .shop-delivery {
-      color: white;
+      color: #666;
     }
     .shop-slogan {
-      color: white;
+      color: #666;
     }
+  }
+  p {
+    color: #666;
   }
   .shop-logo {
     margin-left: 10px;
-    border-radius: 10px;
-    box-shadow: 2px 2px 13px rgba(0,0,0,.5);
+    border-radius: 2px;
   }
   .shop-name {
     margin: 0;
-    font-size: 36px;
-    color: white;
-    font-weight: 600;
+    font-size: 28px;
+    color: #666;
+    font-weight: 500;
+  }
+  .shop-rate {
+    margin-top: 10px;
+  }
+  .img-list {
+    display: flex;
+    width: 400px;
+    li {
+      flex: 0 0 25%;
+    }
   }
 </style>
