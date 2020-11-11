9ecc5e089464d09ff1d30382297667c98c28fff9
MY <715711877@qq.com>
2020-11-11 10:55:06 +0800

    增加 vue.config

--- blb-consumer-frontend/vue.config.js
+++ blb-consumer-frontend/vue.config.js
@@ -1,3 +1,19 @@
+const title = process.env.TITLE
 module.exports = {
-
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title,
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    }
+  }
 }
