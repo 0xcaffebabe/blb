3b5938f3488b7a13b250bacc98d6d0a4e4f8c318
MY <715711877@qq.com>
2020-11-13 11:11:55 +0800

    优化积分商城页面

--- blb-consumer-frontend/src/views/PointsMall.vue
+++ blb-consumer-frontend/src/views/PointsMall.vue
@@ -13,11 +13,11 @@
         </el-radio-group>
         <el-divider/>
         <ul class="product-list">
-          <li class="product-item" v-for="item in 12" :key="item + ''">
-            <el-image src="https://img3.sycdn.imooc.com/5ae18a990001c72c04180342-200-200.jpg"></el-image>
-            <p>黑寡妇第二代RGB机械键盘</p>
-            <p style="color:#ff6600">3926积分</p>
-            <el-button type="success" size="medium">兑换</el-button>
+          <li class="product-item" v-for="item in productList" :key="item.id">
+            <el-image :src="item.img" style="max-width:188px;max-height:144px" fit="cover"></el-image>
+            <p class="product-name">{{item.name}}</p>
+            <p style="color:#ff6600">{{item.point}} 积分</p>
+            <el-button type="success" size="large"  @click="buyProduct">兑换</el-button>
           </li>
         </ul>
       </el-col>
@@ -35,14 +35,14 @@
         <el-card style="margin-top:20px" class="others-buys">
           <div slot="header">TA门在买</div>
           <ul>
-            <li v-for="item in 4" :key="item + ''" class="others-item">
+            <li v-for="item in customerList" :key="item.id" class="others-item">
               <el-row :gutter="20">
                 <el-col :span="6">
-                  <el-avatar src="https://img3.sycdn.imooc.com/5333a0c40001088802000200-100-100.jpg" style="width:48px;height:48px">user</el-avatar>
+                  <el-avatar :src="item.avatar" style="width:48px;height:48px">user</el-avatar>
                 </el-col>
                 <el-col :span="18">
-                  <h3>谁叫我这么坏</h3>
-                  <p>购买了30元全站通用代金券</p>
+                  <h3>{{item.username}}</h3>
+                  <p>兑换了 {{item.product}}</p>
                 </el-col>
               </el-row>
               <el-divider/>
@@ -58,7 +58,61 @@
 export default {
   data () {
     return {
-      productType: '全部'
+      productType: '全部',
+      productList: [],
+      customerList: [],
+      productMetadata: [
+        { name: '黑寡妇第二代RGB机械键盘', img: 'https://img3.sycdn.imooc.com/5ae18a990001c72c04180342-200-200.jpg', point: '3926' },
+        { name: '美的（Midea）扫地机器人', img: 'https://img2.sycdn.imooc.com/5ae186d7000128f403490349.jpg', point: '3245' },
+        { name: '多功能双肩包', img: 'https://img1.sycdn.imooc.com/5aded5bd00011f3a04560447.jpg', point: '595' },
+        { name: '罗技（Logitech）无线鼠标', img: 'https://img4.sycdn.imooc.com/5adecf510001e13304260407.jpg', point: '340' },
+        { name: 'Kindle Paperwhite电纸书阅读器', img: 'https://img2.sycdn.imooc.com/5adef1f20001adb604000447.jpg', point: '3790' }
+      ],
+      personMetadata: [
+        { username: '谁叫我这么坏', avatar: 'https://img3.sycdn.imooc.com/5333a0c40001088802000200-100-100.jpg' },
+        { username: '我不是懂王', avatar: 'https://img2.sycdn.imooc.com/54584d7300011cfa02200220-100-100.jpg' },
+        { username: '铁拳玩家', avatar: 'https://img1.sycdn.imooc.com/54584f540001558402200220-100-100.jpg' },
+        { username: '初见', avatar: 'https://img2.sycdn.imooc.com/5a38d2f4000199fc01000100-100-100.jpg' }
+      ]
+    }
+  },
+  created () {
+    this.generateRandomProduct()
+    this.generateRandomCustomerList()
+  },
+  watch: {
+    productType (val) {
+      this.productType = val
+      this.generateRandomProduct()
+    }
+  },
+  methods: {
+    randomInt (bound) {
+      return Math.ceil(Math.random() * bound) - 1
+    },
+    generateRandomProduct () {
+      const productMetadata = this.productMetadata
+      this.productList = []
+      const n = 12
+      for (let i = 0; i < n; i++) {
+        this.productList.push({ id: i, ...productMetadata[this.randomInt(productMetadata.length)] })
+      }
+    },
+    buyProduct () {
+      this.$message.error({ message: '兑换失败, 积分不足' })
+    },
+    generateRandomCustomerList () {
+      const personMetadata = this.personMetadata
+      const productMetadata = this.productMetadata
+      const n = this.randomInt(4) + 2
+      this.customerList = []
+      for (let i = 0; i < n; i++) {
+        this.customerList.push({
+          id: i,
+          ...personMetadata[this.randomInt(personMetadata.length)],
+          product: productMetadata[this.randomInt(productMetadata.length)].name
+        })
+      }
     }
   }
 }
@@ -96,9 +150,11 @@ export default {
     justify-content: space-around;
     .product-item {
       flex: 0 0 30%;
+      position: relative;
       box-sizing: border-box;
       text-align: center;
       padding: 0 20px;
+      padding-bottom: 0;
       height: 326px;
       width: 100%;
       border: 1px solid #ccc;
@@ -110,9 +166,17 @@ export default {
         width: 100%;
         height: 180px;
       }
+      .product-name {
+        overflow: hidden;
+      }
+      .el-button {
+        position: absolute;
+        margin-left: -50%;
+        height: 48px;
+        font-weight: 600;
+        bottom: 0;
+        width: 100%;
+      }
     }
-  }
-  .product-item:hover {
-    box-shadow: 0 8px 8px 0 rgba(7,17,27,.3);
   }
 </style>
